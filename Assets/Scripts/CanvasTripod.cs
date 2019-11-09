ac9a0bc22a12a38dffda08833bd282b758d10508
eto4detak <fokin91anatoli@gmail.com>
2019-11-09 12:05:48 +0300

    unit HealthBar

---
+++ Assets/Scripts/CanvasTripod.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasTripod : MonoBehaviour
+{
+    private GameObject cam;
+    private float objectScale = 0.1f;
+    private bool orientate = true;
+    private bool scale = false;
+    private Vector3 initialScale;
+    // Use this for initialization
+    void Start()
+    {
+        initialScale = transform.localScale;
+        cam = Camera.main.gameObject;
+    }
+    void Update()
+    {
+        //billboarding the canvas
+        if (orientate)
+        {
+            transform.LookAt(transform.position + cam.transform.rotation * Vector3.back, cam.transform.rotation * Vector3.up);
+            this.transform.Rotate(0, 180, 0);
+        }
+        //making it properly scaled
+        if (scale)
+        {
+            Plane plane = new Plane(cam.transform.forward, cam.transform.position);
+            float dist = plane.GetDistanceToPoint(transform.position);
+            transform.localScale = initialScale * dist * objectScale;
+        }
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Sprite myHealthSpriteBar;
+    public SpriteManager HealthSpriteManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // HealthSpriteManager = GameObject.Find("healthbar_SpriteManager").GetComponent<SpriteManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRotation();
+        UpdateRotation2();
+    }
+
+    private void UpdateRotation()
+    {
+        //if(transform.parent.gameObject.GetComponent<Unit>().Selected == true)
+        //{
+        //    if (myHealthSpriteBar. == true)
+        //    {
+        //        HealthSpriteManager.ShowSprite(myHealthSpriteBar);
+        //        HealthSpriteManager.UpdateBounds();
+        //    }
+
+        //    transform.eulerAngles = new Vector3(
+        //        Camera.main.transform.eulerAngles.x,
+        //        Camera.main.transform.parent.gameObject.transform.eulerAngles.y,
+        //        Camera.main.transform.eulerAngles.z
+        //        );
+        //}
+        //else
+        //{
+        //    if (myHealthSpriteBar.hidden == false)
+        //    {
+        //        HealthSpriteManager.HideSprite(myHealthSpriteBar);
+        //    }
+        //}
+    }
+
+    private void UpdateRotation2()
+    {
+        Debug.Log("test123");
+        var camPosition = Camera.main.transform.position;
+        // camPosition.
+        transform.LookAt(Camera.main.transform.position);
+         var wantedPos = Camera.main.WorldToViewportPoint(WorldManager.character.transform.position);
+        // transform.position = wantedPos;
+        //  transform.rotation.x = 
+    }
+
+}
